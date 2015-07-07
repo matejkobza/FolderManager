c502cf8b4979e408f3a37ea82a473109a1610671
Matej Kobza <mates@ravers.sk>
2015-07-07 11:35:21 +0200

    improvement

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
+<table border="1">
+    <tr>
+        <th>Distribúcia</th>
+        <th>Dátum vytvorenia</th>
+    </tr>
 <?php
 
 $dir = dir(getcwd());
 
 while (false !== ($filename = $dir->read())) {
     if (strpos($filename, '.zip') != false) {
-        echo "<a href=" . $filename . ">" . $filename . "</a><br />";
+        echo "<tr><td><a href=" . $filename . ">" . $filename . "</a></td><td>" . date ("F d Y H:i:s.", filemtime($filename)) . "</td></tr>";
     }
 }
 ?>
+</table>
