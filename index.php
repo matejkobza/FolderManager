5e6e73348e9abdcfe365715d97403d54f0f8787c
Marek Cechvala <cechvalamarek00@gmail.com>
2015-07-14 12:28:58 +0200

    upload

--- index.php
+++ index.php
@@ -9,8 +9,15 @@ $dir = dir(getcwd());
 
 while (false !== ($filename = $dir->read())) {
     if (strpos($filename, '.zip') != false) {
-        echo "<tr><td><a href=" . $filename . ">" . $filename . "</a></td><td>" . date ("F d Y H:i:s.", filemtime($filename)) . "</td></tr>";
+        echo "<tr><td><a href=" . $filename . ">" . $filename . "</a></td><td>" . date ("F d Y H:i:s.", filemtime($filename)) . filesize($filename) . ' bytes' "</td></tr>";
+
+
+
+
     }
 }
 ?>
 </table>
+
+
+$filename =
